113dc93115cd6932c37ad20254eac9814d2e5173
codesparrow6 <57647352+codesparrow6@users.noreply.github.com>
2019-12-05 19:11:12 +0530

    Merge Sort ia=s added

--- Git_Sorting/src/com/java/uttara/Sorting.java
+++ Git_Sorting/src/com/java/uttara/Sorting.java
@@ -4,10 +4,10 @@ public class Sorting {
 
 	// Swapping ...................................................
 	int[] swap(int[] a, int i, int j) {
-		if(i != j) {
-		a[i] = a[i] ^ a[j];
-		a[j] = a[i] ^ a[j];
-		a[i] = a[i] ^ a[j];
+		if (i != j) {
+			a[i] = a[i] ^ a[j];
+			a[j] = a[i] ^ a[j];
+			a[i] = a[i] ^ a[j];
 		}
 		// If data type is integer then we can do it like this also
 		/*
@@ -17,41 +17,43 @@ public class Sorting {
 		return a;
 	}
 
-	//Partition Logic................................................
-	int partition(int[] a,int low,int high) {
-		int left=low,right=high;
+	// QuickSort************************************************************************************************
+	// Partition Logic................................................
+	int partition(int[] a, int low, int high) {
+		int left = low, right = high;
 		int pivot = a[low];
-		
-		while(left < right) {
-			while(left<right && a[left]<=pivot)
+
+		while (left < right) {
+			while (left < right && a[left] <= pivot)
 				left++;
-			
-			while(a[right]>pivot)
+
+			while (a[right] > pivot)
 				right--;
-			
-			if(left<right)
-				swap(a,left,right);
+
+			if (left < right)
+				swap(a, left, right);
 		}
-		
-		swap(a,low,right);
-		
+
+		swap(a, low, right);
+
 		return right;
-		
+
 	}
 
-	int[] quickSort(int[] a,int low,int high) {
-		
-		if(low >= high)
+	int[] quickSort(int[] a, int low, int high) {
+
+		if (low >= high)
 			return null;
 		else {
 			int pIndex = partition(a, low, high);
-            quickSort(a,low,pIndex-1);
-            quickSort(a,pIndex+1,high);
+			quickSort(a, low, pIndex - 1);
+			quickSort(a, pIndex + 1, high);
 		}
 		return a;
 	}
-	
-	
+	// **************************************************************************************************************
+
+	// bubbleSort....................................................................................................
 	int[] bubbleSort(int[] a) {
 		int len = a.length;
 		int i, pass;
@@ -70,6 +72,7 @@ public class Sorting {
 		return a;
 	}
 
+	// SelectionSort.................................................................................................
 	int[] selectionSort(int[] a) {
 		int n = a.length;
 		for (int pos = 0; pos < (n - 1); pos++) {
@@ -83,4 +86,47 @@ public class Sorting {
 		return a;
 	}
 
+	
+	
+	
+	
+	//Merge Sort***************************************************************************************************************
+	
+	int[] merge(int[] a,int low,int high,int[] temp) {
+
+		if(low >= high)
+			return null;
+		else {
+			int mid = (low+high)/2;
+			merge(a,low,mid,temp);
+			merge(a,mid+1,high,temp);
+			
+			mergeSort(a,low,mid,high,temp);
+			
+		}
+		return a;
+	}
+	
+	void mergeSort(int[] a,int low,int mid,int high,int[] temp) {
+		int i=low,j=mid+1,k=low;
+		
+		while(i<=mid && j<=high) {
+			if(a[i] < a[j])
+				temp[k++]=a[i++];
+			else
+				temp[k++]=a[j++];
+		}
+		
+		
+		while(i<=mid)
+			temp[k++]=a[i++];
+		
+		while(j<=high)
+			temp[k++]=a[j++];
+		
+		for(int n=low ;n<=high;n++)
+			a[n]=temp[n];
+		
+	}
+	
 }
--- Git_Sorting/src/com/java/uttara/TestSort.java
+++ Git_Sorting/src/com/java/uttara/TestSort.java
@@ -5,23 +5,26 @@ public class TestSort {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		//NOTE : Dont run all sorting algos together becoz arr gets sorted with first logic and it won't sort afterwards.
-		int [] arr = {-78,-6,-90,80,25,20,30,5,25,30,10,15,-5,23};
-		//int [] arr = {25,30,10,15,5,23};
-		
+		// NOTE : Don't run all sorting algos together becoz array gets sorted with
+		// first logic and it won't sort afterwards.
+		// And we can't verify properly with other Alogs
+		int[] arr = { -78, -6, -90, 80, 25, 20, 30, 5, 25, 30, 10, 15, -5, 23 };
+	    //int [] arr = {25,30,10,15,5,23};
+
 		Sorting bs = new Sorting();
+
+		//int[] a = bs.bubbleSort(arr);
+
+		// int[] b= bs.selectionSort(arr);
+
+		// int[] c= bs.quickSort(arr, 0, arr.length-1);
 		
-		int[] a= bs.bubbleSort(arr);
-		
-		//int[] b= bs.selectionSort(arr);
-		
-		//int[] c= bs.quickSort(arr, 0, arr.length-1);
+		int[] d = bs.merge(arr,0, arr.length-1, new int[arr.length]);
 		System.out.println("Sorting output.............................");
-		
-    	for(int ele:a)
-		 System.out.println(ele+",");
-		
-		
+
+		for (int ele : d)
+			System.out.println(ele + ",");
+
 	}
 
 }
